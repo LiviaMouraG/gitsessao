1eb14c70c3197e6ae02f93c9abf49c354d175cf4
LiviaMouraG <liviamg9@gmail.com>
2023-10-16 11:44:22 -0300

    Commite: Criação da pagina de registro com <h1>

---
+++ resgistro.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de registro</title>
+</head>
+<body>
+    <h1>TESTE REGISTRO</h1>
+</body>
+</html>
